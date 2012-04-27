1fd7fdc9242dc69c7fb4444245eaf240c4da8bec
Mark Evans <mark@new-bamboo.co.uk>
2012-04-27 15:54:57 +0100

    Got rid of view helper, rely on requirejs-rails

--- lib/egg-rails.rb
+++ lib/egg-rails.rb
@@ -1,17 +1,9 @@
-require "egg-rails/version"
-require 'egg-rails/view_helpers'
+require 'egg-rails/version'
+require 'requirejs-rails'
 
 module Egg
   module Rails
-
-    class Railtie < ::Rails::Railtie
-      initializer "egg.view_helpers" do
-        ActionView::Base.send :include, ViewHelpers
-      end
-    end
-
     class Engine < ::Rails::Engine
     end
-
   end
 end
